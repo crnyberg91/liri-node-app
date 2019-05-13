94c4e11d2eccf6d259f7119773256dca58014638
Chris Nyberg <chrisn83@gmail.com>
2019-05-12 23:45:45 -0400

    add bands-in-town function tied to initital prompt

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -15,7 +15,9 @@ inquirer
 		}
 	])
 	.then(inquirerResponse => {
-		switch (inquirerResponse.apiChoice) {
+		switch (
+			inquirerResponse.apiChoice //the apiChoice goes through a switch case associated to the answer
+		) {
 			case "music":
 				inquirer.prompt([
 					{
@@ -27,43 +29,72 @@ inquirer
 				// .then(inquirerResponse => {});
 				//spotify-api-node request prompt code here
 				break;
-			case "a movie":
+			case "a movie": //case for movie choice
 				inquirer
 					.prompt([
 						{
 							type: "input",
-							message: "name of title:",
+							message: "name of title:", //input to store preferred movie
 							name: "title"
 						}
 					])
 					.then(inquirerResponse => {
-						let movie = inquirerResponse.title
-                        if (!inquirerResponse.title){
-                            movie = "mr nobody";
-                        }
-						console.log(movie);
-						let queryUrl =
-							"http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-						console.log(queryUrl);
+						let movie = inquirerResponse.title; //saving title as movie
+						if (!inquirerResponse.title) {
+							//if nothing was typed, it'll default to mr nobody
+							movie = "mr nobody";
+						}
+						// console.log(movie);
+						let queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy"; //movie added to queryUrl for omdb
+						// console.log(queryUrl);
 						axios
 							.get(queryUrl)
 							.then(response => {
+								//displays only specific results about the searched movie in a clean manner on the console
 								console.log(`Title: ${response.data.Title}`);
-                                console.log(`Year released: ${response.data.Year}`);
-                                console.log(`IMDB rating: ${response.data.imdbRating}`);
-                                console.log(`Rotten TOmatoes Rating: ${response.data.Ratings[1].Value}`);
-                                console.log(`Country: ${response.data.Country}`)
-                                console.log(`Language: ${response.data.Language}`);
-                                console.log(`Plot: ${response.data.Plot}`);
-                                console.log(`Actors: ${response.data.Actors}`);
+								console.log(`Year released: ${response.data.Year}`);
+								console.log(`IMDB rating: ${response.data.imdbRating}`);
+								console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+								console.log(`Country: ${response.data.Country}`);
+								console.log(`Language: ${response.data.Language}`);
+								console.log(`Plot: ${response.data.Plot}`);
+								console.log(`Actors: ${response.data.Actors}`);
+							})
+							.catch(err => {
+								//in case of error, the error message will display
+								console.log(err.message);
+							});
+					});
+				break;
+			case "a concert/event":
+				inquirer
+					.prompt([
+						{
+							type: "input",
+							message: "Name of the act you want to see: ",
+							name: "act",
+							validate: function validateAct(name) {
+								return name !== "";
+							}
+						}
+					])
+					.then(inquirerResponse => {
+						let act = inquirerResponse.act;
+						let queryUrl = "https://rest.bandsintown.com/artists/" + act + "/events?app_id=codingbootcamp";
+						// console.log(queryUrl);
+						axios
+							.get(queryUrl)
+							.then(response => {
+								const actDate = response.data[0].datetime.substr(0, 10).split("-").reverse().join("/");
+								const actVenue = response.data[0].venue; //saving venue path as a variable
+								console.log(`name of venue: ${actVenue.name}`); //name of venue
+								console.log(`where: ${(actVenue.city, actVenue.region, actVenue.country)}`); //venue location
+								console.log(`date: ${actDate}`); //date event
 							})
 							.catch(err => {
 								console.log(err.message);
 							});
 					});
-				//omdb axios get request prompt here
-				break;
-			case "a concert/event":
 				//bandsintown axios get request prompt here
 				break;
 			case "surprise me":
